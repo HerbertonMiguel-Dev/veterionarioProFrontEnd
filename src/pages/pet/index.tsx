643b87ccce83a0c9b83c9340f2a3a0560ec28a0a
HerbertonMiguel-Dev <herbertonmiguel.dev@gmail.com>
2024-12-05 19:53:42 -0300

    feito o filtro atraves do telefone do responsavel

--- src/pages/pet/index.tsx
+++ src/pages/pet/index.tsx
@@ -8,13 +8,15 @@ import {
   Stack,
   Switch,
   useMediaQuery,
+  Input,
+  IconButton,
   Center,
 } from '@chakra-ui/react';
 import Link from 'next/link';
 
-import { FaPaw, } from 'react-icons/fa';
+import { FaPaw, FaSearch } from 'react-icons/fa';
 
-import  IconCombo  from '../../components/icon'
+import IconCombo from '../../components/icon';
 
 import { canSSRAuth } from '../../utils/canSSRAuth';
 import { Sidebar } from '../../components/sidebar';
@@ -39,7 +41,7 @@ interface PetItem {
 
 interface PetPros {
   pets: PetItem[];
-  responsaveis: ResponsavelItem
+  responsaveis: ResponsavelItem;
 }
 
 export default function Pets({ pets, responsaveis }: PetPros) {
@@ -47,6 +49,16 @@ export default function Pets({ pets, responsaveis }: PetPros) {
   const [petList, setPetList] = useState<PetItem[]>(pets || []);
   const [responsaveisList, setResponsaveisList] = useState<ResponsavelItem[]>(responsaveis ? [responsaveis] : []);
   const [disablePet, setDisablePet] = useState('enabled');
+  const [searchPhone, setSearchPhone] = useState('');
+  const [filteredPets, setFilteredPets] = useState<PetItem[] | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  const [showList, setShowList] = useState(true);
+
+  const toggleListVisibility = (e: ChangeEvent<HTMLInputElement>) => {
+    setShowList(e.target.checked);
+  };
+
 
   async function handleDisable(e: ChangeEvent<HTMLInputElement>) {
     const apiClient = configurarClienteAPI();
@@ -62,6 +74,26 @@ export default function Pets({ pets, responsaveis }: PetPros) {
     }
   }
 
+  const handleSearch = async () => {
+    if (!searchPhone) {
+      alert('Por favor, insira o telefone do responsável para buscar.');
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      const apiClient = configurarClienteAPI();
+      const response = await apiClient.get(`/pets/search?telefone=${searchPhone}`);
+      setFilteredPets(response.data);
+    } catch (error) {
+      console.error('Erro ao buscar pets:', error);
+      alert('Não foi possível buscar os pets. Verifique o número e tente novamente.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <>
       <Head>
@@ -69,6 +101,7 @@ export default function Pets({ pets, responsaveis }: PetPros) {
       </Head>
       <Sidebar>
         <Flex direction="column" alignItems="flex-start" justifyContent="flex-start">
+          {/* Cabeçalho */}
           <Flex
             direction={isMobile ? 'column' : 'row'}
             w="100%"
@@ -90,114 +123,171 @@ export default function Pets({ pets, responsaveis }: PetPros) {
                 Cadastrar novo
               </Button>
             </Link>
-            <Stack ml="auto" align="center" direction="row">
-              <Text fontWeight="bold" color="#fff">
-                {disablePet === 'disabled' ? 'DESATIVADOS' : 'ATIVOS'}
-              </Text>
-              <Switch
-                colorScheme="green"
-                size="lg"
-                value={disablePet}
-                onChange={(e: ChangeEvent<HTMLInputElement>) => handleDisable(e)}
-                isChecked={disablePet === 'disabled' ? false : true}
-              />
-            </Stack>
-          </Flex>
-          {petList.map((pet) =>
-            pet.responsaveis.map((responsavel) => (
-              <Link
-                key={`${pet.id}-${responsavel.id}`}
-                href={`/pet/${pet.id}/${responsavel.id}`}
-                style={{ width: '100%' }}
-              >
+            <Stack ml="auto" align="center" direction="column" spacing={4}>
+              {/* Primeiro Switch */}
+              <Flex align="center">
+                <Text fontWeight="bold" color="#fff" mr={4}>
+                  {disablePet === 'disabled' ? 'DESATIVADOS' : 'ATIVOS'}
+                </Text>
+                <Switch
+                  colorScheme="green"
+                  size="lg"
+                  value={disablePet}
+                  onChange={(e: ChangeEvent<HTMLInputElement>) => handleDisable(e)}
+                  isChecked={disablePet === 'disabled' ? false : true}
+                />
+              </Flex>
 
-                <Flex
-                  cursor="pointer"
-                  w="70%"
-                  p={4}
-                  bg="veterinario.400"
-                  direction={isMobile ? 'column' : 'row'}
-                  align={isMobile ? 'flex-start' : 'center'}
-                  rounded="4"
-                  mb={2}
-                  justifyContent="flex-start"
-                  
-                >
-                    <Flex mr={5}>
-                      <FaPaw  size={20} color="#000" />
-                    </Flex>
-                  <Flex direction="column" mr={4}>
-                    <Flex mr={40} direction="row" mb={isMobile ? 2 : 0} align="center" justify="center">
-                      <Flex direction="column" w="100%">
+              {/* Segundo Switch */}
+              <Flex align="center">
+                <Text fontWeight="bold" color="#fff" mr={4}>
+                  {showList ? 'Ocultar Pets' : 'Mostrar Pets'}
+                </Text>
+                <Switch
+                  colorScheme="green"
+                  size="lg"
+                  isChecked={showList}
+                  onChange={toggleListVisibility}
+                />
+              </Flex>
+            </Stack>
+
+          </Flex>
+
+          {/* Área de busca */}
+          <Flex
+            direction={isMobile ? 'column' : 'row'}
+            w="40%"
+            align="center"
+            bg="veterinario.400"
+            p={2}
+            borderRadius="md"
+            mb={2}
+          >
+            <Input
+              placeholder="Telefone do responsável"
+              size="lg"
+              type="text"
+              value={searchPhone}
+              onChange={(e) => setSearchPhone(e.target.value)}
+              bg="veterinario.900"
+              color="#fff"
+              mr={isMobile ? 0 : 4}
+              mb={isMobile ? 4 : 0}
+            />
+            <IconButton
+              aria-label="Buscar"
+              icon={<FaSearch />}
+              size="lg"
+              bg="button.cta"
+              _hover={{ bg: '#FFb13e' }}
+              onClick={handleSearch}
+              isLoading={loading}
+            />
+          </Flex>
+
+          {/* Lista de pets */}
+          {(showList || filteredPets) && (
+            <>
+              {(filteredPets || petList).map((pet) =>
+                pet.responsaveis.map((responsavel) => (
+                  <Link
+                    key={`${pet.id}-${responsavel.id}`}
+                    href={`/pet/${pet.id}/${responsavel.id}`}
+                    style={{ width: '100%' }}
+                  >
+                    <Flex
+                      cursor="pointer"
+                      w="50%"
+                      p={4}
+                      bg="veterinario.400"
+                      direction={isMobile ? 'column' : 'row'}
+                      align={isMobile ? 'flex-start' : 'center'}
+                      rounded="4"
+                      mb={2}
+                      justifyContent="flex-start"
+                    >
+                      <Flex mr={5}>
+                        <FaPaw size={20} color="#000" />
+                      </Flex>
+                      <Flex direction="column" mr={4}>
+                        <Flex mr={40} direction="row" mb={isMobile ? 2 : 0} align="center" justify="center">
+                          <Flex direction="column" w="100%">
+                            <Text color="#fff" fontWeight="semibold">
+                              <Text mr={2} as="span" color="#000" fontWeight="bold">
+                                Nome:
+                              </Text>
+                              {pet.nome}
+                            </Text>
+                          </Flex>
+                        </Flex>
                         <Text color="#fff" fontWeight="semibold">
                           <Text mr={2} as="span" color="#000" fontWeight="bold">
-                            Nome:
+                            Espécie:
                           </Text>
-                          {pet.nome}
+                          {pet.tipo}
                         </Text>
+                        <Text color="#fff" fontWeight="semibold">
+                          <Text mr={2} as="span" color="#000" fontWeight="bold">
+                            Raça:
+                          </Text>
+                          {pet.raca || 'Não especificada'}
+                        </Text>
+
+                        <Text color="#fff" fontWeight="semibold">
+                          <Text mr={2} as="span" color="#000" fontWeight="bold">
+                            Idade:
+                          </Text>
+                          {pet.idade
+                            ? `${pet.idade} ${pet.idade > 1 ? 'anos' : 'ano'}`
+                            : 'Idade não informada'}
+                        </Text>
+
+                        <Text color="#fff" fontWeight="semibold">
+                          <Text mr={2} as="span" color="#000" fontWeight="bold">
+                            Peso:
+                          </Text>
+                          {pet.peso ? `${pet.peso} Kg` : 'Idade não informada'}
+                        </Text>
+
+                      </Flex>
+                      <Flex>
+                        <IconCombo size={`${30}px`} />
+                      </Flex>
+                      <Flex direction="column" align="flex-start">
+                        <Text color="#fff" fontWeight="semibold">
+                          <Text mr={2} as="span" color="#000" fontWeight="bold">
+                            Responsável:
+                          </Text>
+                          {responsavel.nome}
+                        </Text>
+
+                        <Text color="#fff" fontWeight="semibold">
+                          <Text mr={2} as="span" color="#000" fontWeight="bold">
+                            Telefone:
+                          </Text>
+                          {responsavel.telefone}
+                        </Text>
+
+                        <Text color="#fff" fontWeight="semibold">
+                          <Text mr={2} as="span" color="#000" fontWeight="bold">
+                            Endereço:
+                          </Text>
+                          {responsavel.endereco}
+                        </Text>
+
                       </Flex>
                     </Flex>
-                    <Text color="#fff" fontWeight="semibold">
-                      <Text mr={2} as="span" color="#000" fontWeight="bold">
-                        Espécie:
-                      </Text>
-                      {pet.tipo}
-                    </Text>
-                    <Text color="#fff" fontWeight="semibold">
-                      <Text mr={2} as="span" color="#000" fontWeight="bold">
-                        Raça:
-                      </Text>
-                      {pet.raca || 'Não especificada'}
-                    </Text>
-                    <Text color="#fff" fontWeight="semibold">
-                      <Text mr={2} as="span" color="#000" fontWeight="bold">
-                        Idade:
-                      </Text>
-                      {pet.idade
-                        ? `${pet.idade} ${pet.idade > 1 ? 'anos' : 'ano'}`
-                        : 'Idade não informada'}
-                    </Text>
-                    <Text color="#fff" fontWeight="semibold">
-                      <Text mr={2} as="span" color="#000" fontWeight="bold">
-                        Peso:
-                      </Text>
-                      {pet.peso ? `${pet.peso} Kg` : 'Idade não informada'}
-                    </Text>
-
-                  </Flex>
-                    <Flex >
-                      <IconCombo size={`${30}px`} />
-                    </Flex>
-                  <Flex direction="column" align="flex-start" >
-                    <Text color="#fff" fontWeight="semibold">
-                      <Text mr={2} as="span" color="#000" fontWeight="bold">
-                        Responsável:
-                      </Text>
-                      {responsavel.nome}
-                    </Text>
-                    <Text color="#fff" fontWeight="semibold">
-                      <Text mr={2} as="span" color="#000" fontWeight="bold">
-                        Telefone:
-                      </Text>
-                      {responsavel.telefone}
-                    </Text>
-                    <Text color="#fff" fontWeight="semibold">
-                      <Text mr={2} as="span" color="#000" fontWeight="bold">
-                        Endereço:
-                      </Text>
-                      {responsavel.endereco}
-                    </Text>
-                  </Flex>
-
-                </Flex>
-              </Link>
-            ))
+                  </Link>
+                ))
+              )}
+              </>
           )}
-
-        </Flex>
-      </Sidebar>
+            </Flex >
+      </Sidebar >
     </>
   );
+
 }
 
 export const getServerSideProps = canSSRAuth(async (ctx) => {
@@ -229,3 +319,4 @@ export const getServerSideProps = canSSRAuth(async (ctx) => {
     };
   }
 });
+``
